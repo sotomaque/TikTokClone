1c39846939d4f58785aa2dc2667796fa772b99c6
Enrique <sotomaque@gmail.com>
2021-02-22 15:42:15 -0800

    finishe amplify project setup with auth and api services; configured lambda to run on post confirmation

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {SafeAreaView, StatusBar} from 'react-native';
+import {withAuthenticator} from 'aws-amplify-react-native';
 
 import Navigation from './src/navigation';
+
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 const App: () => React$Node = () => {
   return (
@@ -15,4 +20,4 @@ const App: () => React$Node = () => {
   );
 };
 
-export default App;
+export default withAuthenticator(App);
